13d8b3535797fdaee95ab750071d5fdaa2803f8a
Emmanuel (Maxzeno) <mmnlchidera@gmail.com>
2022-11-20 02:14:22 +0100

    debug false

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # USE LOCAL STORAGE
 TRY_LOCAL_ = False
