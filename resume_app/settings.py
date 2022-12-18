ea24037ef0a660c38e2484e8156e7b146f19f7da
Emmanuel (Maxzeno) <mmnlchidera@gmail.com>
2022-12-18 20:47:53 +0100

    the app shouldn't update the db to log on commit

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'resume_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3' if DEBUG else BASE_DIR / 'db-production.sqlite3',
     }
 }
 
