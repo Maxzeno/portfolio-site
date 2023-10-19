010ffa4ca371e77d066a0add5b89261a2df85f4b
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-10-19 20:52:24 +0100

    added app = application

--- resume_app/wsgi.py
+++ resume_app/wsgi.py
@@ -13,3 +13,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_app.settings')
 
 application = get_wsgi_application()
+
+# vercel
+app = application
